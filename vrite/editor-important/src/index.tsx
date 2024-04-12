f7d1cd3c62bdc914af2641ec9392588e6cfaa359
Arek Nawo <areknawo@areknawo.com>
2024-04-12 23:55:49 +0200

    wip: Editor extension

--- vrite/editor-important/src/index.tsx
+++ vrite/editor-important/src/index.tsx
@@ -22,24 +22,85 @@ const alertIcon =
 export default createRuntime({
   elements: [
     {
-      type: "Contract",
-      view: createView<ExtensionElementViewContext<Config>>(({ content }) => {
+      type: "Issue",
+      view: createView<
+        ExtensionElementViewContext<
+          Config,
+          { severity: "informational" | "low" | "medium" | "high" | "critical" }
+        >
+      >(({ use, css }) => {
+        const [severity, setSeverity] = use("props.severity");
+
+        /*if (!severity()) {
+          setSeverity("informational");
+        }*/
+
         return (
-          <Components.Card class="flex flex-col items-center justify-start p-3 m-0 my-4">
-            {content}
-          </Components.Card>
-        );
-      }),
-    },
-    {
-      type: "Title",
-      view: createView<ExtensionElementViewContext<Config>>(({ content }) => {
-        return (
-          <Components.View class="flex -my-4">
-            <Components.View class="text-gray-500 dark:text-gray-400 mr-1">
-              Title:
-            </Components.View>
-            {content}
+          <Components.View
+            class={css`flex flex-col items-center justify-start m-0 my-4 rounded-2xl border-2 border-gray-200 dark:border-gray-700`}
+          >
+            <Components.Content>
+              <Components.Element type="Title">
+                <Components.View
+                  class={css`flex items-start border-b-2 border-gray-200 dark:border-gray-700 bg-gray-50 rounded-t-2xl`}
+                >
+                  <Components.View
+                    class={css`mr-1 text-gray-500 dark:text-gray-400 font-bold text-base h-[35px] font-mono min-w-40 px-2 inline-flex items-center`}
+                  >
+                    Issue
+                  </Components.View>
+                  <Components.Content content="paragraph" />
+                </Components.View>
+              </Components.Element>
+              <Components.Element type="Severity">
+                <Components.View
+                  class={css`flex items-start border-b-2 border-gray-200 dark:border-gray-700`}
+                >
+                  <Components.View
+                    class={css`text-gray-500 dark:text-gray-400 mr-1 font-bold text-base h-10 font-mono min-w-40 px-2 inline-flex items-center`}
+                  >
+                    Severity
+                  </Components.View>
+                  <Components.View class={css`flex items-center`}>
+                    <Components.Select
+                      bind:value={severity}
+                      class="mx-0"
+                      options={[
+                        { label: "Informational", value: "informational" },
+                        { label: "Low", value: "low" },
+                        { label: "Medium", value: "medium" },
+                        { label: "High", value: "high" },
+                        { label: "Critical", value: "critical" },
+                      ]}
+                    />
+                  </Components.View>
+                </Components.View>
+              </Components.Element>
+              <Components.Element type="Description">
+                <Components.View
+                  class={css`flex items-start border-b-2 py-2 border-gray-200 dark:border-gray-700`}
+                >
+                  <Components.View
+                    class={css`text-gray-500 dark:text-gray-400 mr-1 font-bold text-base h-[35px] font-mono min-w-40 px-2 inline-flex items-center`}
+                  >
+                    Description
+                  </Components.View>
+                  <Components.Content content="paragraph+" />
+                </Components.View>
+              </Components.Element>
+              <Components.Element type="Recommendation">
+                <Components.View
+                  class={css`flex items-start py-2 border-gray-200 dark:border-gray-700 rounded-b-2xl`}
+                >
+                  <Components.View
+                    class={css`text-gray-500 dark:text-gray-400 mr-1 font-bold text-base h-[35px] font-mono min-w-40 px-2 inline-flex items-center`}
+                  >
+                    Recommendation
+                  </Components.View>
+                  <Components.Content content="paragraph+" />
+                </Components.View>
+              </Components.Element>
+            </Components.Content>
           </Components.View>
         );
       }),
